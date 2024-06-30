17a9af2afe7fa99f2bcb33fbd0c0355c69bf486b
Adam Flitney <adam.flitney@gmail.com>
2024-06-30 13:03:25 +0100

    generate a basic short id

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"net/http"
+
+	"github.com/teris-io/shortid"
 )
 
 func main() {
 	fmt.Println("hello world!")
+
+	urlMappings := make(map[string]string)
 
 	router := http.NewServeMux()
 
@@ -15,7 +21,17 @@ func main() {
 	})
 
 	router.HandleFunc("POST /short-url/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello from post"))
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		newId, err := shortid.Generate()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		urlMappings[newId] = string(body)
+
+		w.Write([]byte(newId))
 	})
 
 	fs := http.FileServer(http.Dir("./web"))
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/adamflitney/short-url
 
 go 1.22.4
+
+require github.com/teris-io/shortid v0.0.0-20220617161101-71ec9f2aa569 // indirect
