f178905750477b588d62573fbcb7fc278cde6f9f
Adam Flitney <adam.flitney@gmail.com>
2024-06-30 12:45:29 +0100

    basic routing

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("hello world!")
+
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /short-url/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello from get"))
+	})
+
+	router.HandleFunc("POST /short-url/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello from post"))
+	})
+
+	fs := http.FileServer(http.Dir("./web"))
+
+	router.Handle("/", fs)
+
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	server.ListenAndServe()
 }
